bcf72f1c5db9c9c91b448adce1df2ee90f173d4a
Eduardo5011 <eduardo.rosado5011@gmail.com>
2024-12-01 19:36:10 -0500

    updated navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import { CartIcon } from "../icons";
+import { useSelector } from "react-redux";
+
+const navbar = () => {
+  return (
+    <nav>
+        <div className="nav-center">
+            <h3>Redux toolkit</h3>
+        </div>
+    </nav>
+  )
+}
+export default navbar
