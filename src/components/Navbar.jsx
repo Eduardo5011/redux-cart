ca19d05c4a859c46ed51f115dfadbfa7c37927f2
Eduardo5011 <eduardo.rosado5011@gmail.com>
2024-12-07 19:58:24 -0500

    udpated navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,6 +6,12 @@ const navbar = () => {
     <nav>
         <div className="nav-center">
             <h3>Redux toolkit</h3>
+            <div className="nav-container">
+          <CartIcon/>
+          <div className="amount-container">
+            <p className="total-amount">0</p>
+          </div>
+            </div>
         </div>
     </nav>
   )
