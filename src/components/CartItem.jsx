6a65ad78d2e8405e89d0bebf1d5867e7496a49e3
Eduardo5011 <eduardo.rosado5011@gmail.com>
2024-12-23 19:47:33 -0500

    updated CartItem

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -23,13 +23,22 @@ const CartItem = ({ id, img, title, price, amount }) => {
         <button
           className="amount-btn"
           onClick={() => {
-            dispatch(increase(id));
+            dispatch(increase({ id }));
           }}
         >
           <ChevronUp />
         </button>
         <p className="amount">{amount}</p>
-        <button className="amount-btn">
+        <button
+          className="amount-btn"
+          onClick={() => {
+            if(amount === 1){
+              dispatch(removeItem(id))
+              return;
+            }
+            dispatch(decrease({ id }));
+          }}
+        >
           <ChevronDown />
         </button>
       </div>
