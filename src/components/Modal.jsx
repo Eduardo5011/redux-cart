ed91d03c8c285780df4001c92ab3053d77b7a2a7
Eduardo5011 <eduardo.rosado5011@gmail.com>
2024-12-29 16:30:49 -0500

    created modal.jsx and modal.js

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Modal = () => {
   return (
     <aside className="modal-container">
-      <div className="modal-container">
+      <div className="modal">
         <h4>remove all items from your shopping cart?</h4>
         <div className="btn-container">
           <button type="button" className="btn confirm-btn">
